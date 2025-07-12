9e2d60f438ba3bb740efb81b96ccc6bfd07402fb
Mikita <ghjhjr11@gmail.com>
2025-07-12 17:11:56 +0300

    swagger done

--- src/server.js
+++ src/server.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { pinoHttp } from 'pino-http';
+// import { pinoHttp } from 'pino-http';
 import cors from 'cors';
 import { getEnvVar } from './utils/getEnvVar.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import router from './routers/index.js';
 import cookieParser from 'cookie-parser';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 const PORT = +getEnvVar('PORT');
 
@@ -15,6 +16,7 @@ export const setupServer = () => {
   app.use(express.json());
   app.use(cors());
   app.use(cookieParser());
+  app.use('/api-docs', swaggerDocs());
 
   // app.use(
   //   pinoHttp({
