c6ce15783f55a7367d831dd7251b484d5b5a2189
Mikita <ghjhjr11@gmail.com>
2025-07-13 23:29:00 +0300

    swagger fix 5

--- src/middlewares/checkIsOwnerOfId.js
+++ src/middlewares/checkIsOwnerOfId.js
@@ -2,6 +2,11 @@ import createHttpError from 'http-errors';
 import { ContactsCollection } from '../db/models/contacts.js';
 
 export const checkIsOwnerOfId = async (req, res, next) => {
+  if (!(await ContactsCollection.findById(req.params.contactId))) {
+    next(createHttpError(404, 'Contact not found'));
+    return;
+  }
+
   const contact = await ContactsCollection.findOne({
     userId: req.user._id,
     _id: req.params.contactId,
